cf85e5e3873d2131bc060ec54ebd01db51455ad0
Patrycja <patrycjatyra@protonmail.com>
2020-01-09 16:33:21 +0000

    Create hokuspokus.js

---
+++ hokuspokus.js
@@ -0,0 +1,22 @@
+function calc() {
+    let a = parseInt(document.querySelector("#value1").value);
+    let b = parseInt(document.querySelector("#value2").value);
+    let op = document.querySelector("#operator").value;
+
+    let calculate;
+
+
+    if (op == "add") {
+        calculate = a + b;
+    } else if (op == "min") {
+        calculate = a - b;
+    } else if (op == "div") {
+        calculate = a / b;
+    } else if (op == "multi") {
+        calculate = a * b;
+    }
+
+
+    document.querySelector("#result").innerHTML = calculate;
+
+}
